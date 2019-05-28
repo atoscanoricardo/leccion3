87780f75f624a12492fd8c88e3e1b35be000725f
Alexander Toscano Ricardo <atoscano@correo.unicordoba.edu.co>
2019-05-28 12:12:20 +0000

    Init server

--- bin/Controller.js
+++ bin/Controller.js
@@ -4,27 +4,35 @@ const  User  = require("./models/User");
 class Controller{
 
     constructor(){
+        //al crearse el objeto se establece la conexion
         this.connect();
     }
 
     async connect(){
        try {
-      await mongoose.connect(
-            "mongodb+srv://kikret:7x7e9l1a@cluster0-9ek42.mongodb.net/myMusicDB?retryWrites=true",
-            { useNewUrlParser: true }
-        );
+        //se intenta establecer una conexion con los datos de conexion
+         await mongoose.connect(
+                    "mongodb+srv://kikret:7x7e9l1a@cluster0-9ek42.mongodb.net/myMusicDB?retryWrites=true",
+                    { useNewUrlParser: true }
+                );
+            //en caso de esablecer la conexion, se muestra en consola este mensaje
             console.log("Connected databases.");
         } catch (e) {
+            //en caso de haber un error se muestra en consola el error
             console.error(e);
         }
     }
-
+    //metodo para buscar todos los usuarios
     getUsers(){
+        //en el modelo User se ejecuta el find sin ninguna condicion...
         User.find({}, (err, users)=>{
+            //en caso de haberse presentado un error se ejecuta el error
+            if(err) throw err;
+            //de lo contrario se retorna un objeto con todos los resultados
             res.send( { allUsers : users } );
         })
     }
 }
-
+// se exporta elobjeto controlador crado a partir de la clase Controller
 exports.controller = new Controller();
 
--- bin/routes.js
+++ bin/routes.js
@@ -1,16 +1,21 @@
+//requerimos las biblotecas express para las rutas, bodyParser para loenvios por post de JSON
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const controller = require("./Controller");
 app.use(bodyParser.json());
+//llamamos el controlador que se encarga de gestionar la base de datos
+const controller = require("./Controller");
 
-
+//creamos la ruta raiz para enviar un mensaje de bienvenida con la version
 app.get("/", (req, resp)=>{
+    //retornamos un mensaje
     res.send("My Music v1")
 })
-
+//creamos la ruta users que deberá traer todos los usuarios
 app.get("/users", (req, resp)=>{
+    //llamamos el metodo getUser del objeto controller, este se encarga de buscar todos los usuarios
+    //recibe por parametros req que es igual a la consulta request(consulta) y el res que equivale al response(respuesta)
     controller.getUsers(req, resp);
 })
-
+//exportamos la constante app con toda la configuracion de las rutas
 exports.app = app
